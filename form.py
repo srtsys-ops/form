9efc588d3d08262a610504d8e7fd927e9b021be9
srtsys-ops <srtsys@gmail.com>
2026-03-28 11:16:29 +0530

    Add files via upload

--- form.py
+++ form.py
@@ -3,8 +3,12 @@ import streamlit as st
 
 diabetes_model = pickle.load(open("diabetes_model.sav", "rb"))
 
+# input_data = [[
+#     6,178,90,35,220,34.60,1.450,62
+# ]]
+
 input_data = [[
-    6,178,90,35,220,34.60,1.450,62
+    3,126,88,41,235,39.30,0.704,27
 ]]
 
 diab_prediction = diabetes_model.predict(input_data)
